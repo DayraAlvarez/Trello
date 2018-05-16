f38a2e47abc0268ac22249935688c084daf632ad
DayraAlvarez <dayra.g.alvarez@gmail.com>
2018-05-16 12:26:16 -0500

    Avance de codigo en JS, verifica si no se ingresa texto

--- js/app.js
+++ js/app.js
@@ -1,24 +1,25 @@
-var person = prompt("Please enter your name");
+var hola = alert("Please enter your name");
 // Al hacer click en boton
 var startAdding =  document.getElementById('new-task-button');
+var addList = document.getElementById('nav-txt').value;
 //Tomar texto ingresado
 startAdding.addEventListener('click', function(){
-  var addList = document.getElementById('nav-txt').value;
   document.getElementById('nav-txt').value = '';
-//Contenedor en HTML
+  //Contenedor en HTML
   var cont = document.getElementById('cont');
-//Crear contenido en div
-	var newList = document.createElement('div');
-//Validar ingresar texto
-if(addList.length == 0 || addList == null) {
-  alert('Ingresa un nombre de lista');
-  return false;
-}
-//Creaicion de nodos de texto en html
-var yourNewList = document.createElement(addList);
+  //Crear contenido en div
+  var newList = document.createElement('div');
+  //Validar ingresar texto
+  if(addList.length == 0 || addList == null) {
+    alert('Ingresa un nombre de lista');
+    return false;
+  }
+});
+//Creacion de nodos de texto en html
+//var yourNewList = document.createElement(addList);
 
 var elementContainer = document.createElement('p');
-elementContainer.appendChild(yourNewList);
-newList.appendChild(elementContainer);
+//elementContainer.appendChild(yourNewList);
+//newList.appendChild(elementContainer);
 
-cont.appendChild(newList);
+//cont.appendChild(newList);
