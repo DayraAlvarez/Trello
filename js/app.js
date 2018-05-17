99e014cdbbe20bc4eb1d64663a4f7c9fccaf3748
DayraAlvarez <dayra.g.alvarez@gmail.com>
2018-05-17 14:07:07 -0500

    Funcionalidad de form

--- js/app.js
+++ js/app.js
@@ -12,5 +12,26 @@
     document.getElementById('add-list-form').style.display="none";
     var listTitleValue=document.getElementById('new-list-title').value;
     document.getElementById('list-title').textContent=listTitleValue;
-    document.getElementById('add-task').style.display="block";
+    document.getElementById('task').style.display="block";
+
+  });
+
+
+  var addTask = document.getElementById('task');
+    addTask.addEventListener('click', function(){
+    document.getElementById('new-task').style.display="block";
+    addTask.style.display="none";
+  });
+
+  var saveTask= document.getElementById('save-new-task-button');
+  saveTask.addEventListener('click',function(){
+    var createNewTask=document.getElementById('save-new-task-content').value;
+    var newTask=document.createElement('p');
+    newTask.textContent=createNewTask;
+     document.getElementById('father').appendChild(newTask);
+      //obtener el valor del input
+      //CREAR UN NUEVO P CON ESE CONTENIDO
+      //inserarlo
+      //VACIAR EL CONTENIDO DEL FORMULARIO
+      //DEVOLVER EL FOCUS AL Formulario
   });
