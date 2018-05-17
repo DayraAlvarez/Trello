0bcd27c6e38665badfea650d6db68b7a5b0179c5
DayraAlvarez <dayra.g.alvarez@gmail.com>
2018-05-17 12:01:24 -0500

    Arreglo de codigo js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,39 @@
-var a = document.getElementById('nav-txt');
 
-function create (event) {
-  var gettingStarted =document.getElementById('getting-started');
-  var secondValue =document.getElementById('second-value');
-  secondValue.style.visibility='visible';
-  gettingStarted.style.visibility='hidden';
-}
 
-a.addEventListener('click', create);
+  var addList = document.getElementById('add-list');
+  addList.addEventListener('click', function(){
+    addList.style.display="none";
+    document.getElementById('add-list-form').style.display="block";
+  });
 
-var saveButton =document.getElementById('new-list-button');
-saveButton.addEventListener('click', createList);
 
-function createList (){
-  var listTittleValue=document.getElementById('second-box').value;
-  var newListTitle = document.getElementById('new-list-title');
-  newListTitle.value = listTittleValue;
-  document.getElementById('second-box').value = '';
+  var saveList =document.getElementById('save-list');
+  saveList.addEventListener('click', function(){
+    document.getElementById('add-list-form').style.display="none";
+    var listTitleValue=document.getElementById('new-list-title').value;
+    document.getElementById('list-title').textContent=listTitleValue;
+  });
 
-}
+
+
+
+
+  function create (event) {
+    var gettingStarted =document.getElementById('getting-started');
+    var secondValue =document.getElementById('second-value');
+    secondValue.style.visibility='visible';
+    gettingStarted.style.visibility='hidden';
+  }
+
+  a.addEventListener('click', create);
+
+  var saveButton =document.getElementById('new-list-button');
+  saveButton.addEventListener('click', createList);
+
+  function createList (){
+    var listTittleValue=document.getElementById('second-box').value;
+    var newListTitle = document.getElementById('new-list-title');
+    newListTitle.value = listTitleValue;
+    document.getElementById('second-box').value = '';
+
+  }
